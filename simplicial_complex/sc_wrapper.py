f5a7e390ba94133ce93f598c42b7f38922e91d12
sordonia120446 <samuel.ordonia@gmail.com>
2017-03-21 02:01:35 -0700

    wrapper rev0

--- simplicial_complex/sc_wrapper.py
+++ simplicial_complex/sc_wrapper.py
@@ -2,6 +2,19 @@ from ctypes import *
 
 lib = CDLL('./cpp/SCExport.so')
 
-print(lib)
+class SimplicialComplex(object):
 
+    def __init__(self, num_of_rules, threshold, cols, rows, filepath=None):
+        self.obj = lib.createInstance()
+        self.num_of_rules = num_of_rules
+        self.threshold = threshold
+        self.cols = cols
+        self.rows = rows
+        self.filepath = filepath
 
+    def set_bit_map_row(self, row_data, bit_vector):
+        for ind, row in enumerate(row_data):
+            lib.setBitMapRow(self.obj, self.cols, ind, row)
+
+    def process(self):
+        lib.process(self.obj)
